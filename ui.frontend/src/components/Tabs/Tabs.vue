04a8be897ea054dd2f4f32c9030e16f04d60f26f
Samuel Iatarola <samueliatarola@gmail.com>
2022-04-09 13:26:54 -0300

    Feat: added location reload to the page

--- ui.frontend/src/components/Tabs/Tabs.vue
+++ ui.frontend/src/components/Tabs/Tabs.vue
@@ -12,13 +12,16 @@ export default {
   name: 'Tabs',
   methods: {
     handleBasic () {
-      router.push({ name: 'basic' })
+      router.push({name: 'basic'})
+      location.reload()
     },
     handleSocial () {
-      router.push({ name: 'social' })
+      router.push({name: 'social'})
+      location.reload()
     },
     handleCertificates () {
-      router.push({ name: 'certificates' })
+      router.push({name: 'certificates'})
+      location.reload()
     }
   }
 }
